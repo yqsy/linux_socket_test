62d2e31392ec2bb6e92db52ba9ccd7bab07dc5e9
yqsy <yqsy021@126.com>
2017-11-09 14:47:25 +0800

    update

--- check_byte_order.cpp
+++ check_byte_order.cpp
@@ -10,6 +10,7 @@ void byteorder()
     test.value = 0x0102;
     if ((test.union_bytes[0] == 1) && (test.union_bytes[1] == 2))
     {
+
         printf("big endian\n");
     }
     else if ((test.union_bytes[0] == 2) && (test.union_bytes[1] == 1))
---
+++ readme.md
@@ -0,0 +1,15 @@
+
+<!-- TOC -->
+
+- [1. 编译](#1-编译)
+
+<!-- /TOC -->
+
+
+<a id="markdown-1-编译" name="1-编译"></a>
+# 1. 编译
+
+```bash
+g++ check_byte_order.cpp -o check_byte_order
+
+```
