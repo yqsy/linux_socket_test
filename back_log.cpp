ced9dd161e1dc7d4966e5f4e3f38dbae7b3ce610
yqsy <yqsy021@126.com>
2017-11-09 14:48:44 +0800

    update

--- back_log.cpp
+++ back_log.cpp
@@ -17,7 +17,7 @@ static void handle_term(int sig)
 int main(int argc, char *argv[])
 {
     signal(SIGTERM, handle_term);
-    if (argc＜ = 3)
+    if (argc <= 3)
     {
         printf("usage:%s ip_address port_number backlog\n", basename(argv[0]));
         return 1;
@@ -26,14 +26,14 @@ int main(int argc, char *argv[])
     int port = atoi(argv[2]);
     int backlog = atoi(argv[3]);
     int sock = socket(PF_INET, SOCK_STREAM, 0);
-    assert(sock> = 0);
+    assert(sock > = 0);
     /*创建一个IPv4 socket地址*/
     struct sockaddr_in address;
-    bzero(＆address, sizeof(address));
+    bzero(&address, sizeof(address));
     address.sin_family = AF_INET;
-    inet_pton(AF_INET, ip, ＆address.sin_addr);
+    inet_pton(AF_INET, ip, &address.sin_addr);
     address.sin_port = htons(port);
-    int ret = bind(sock, (struct sockaddr *)＆address, sizeof(address));
+    int ret = bind(sock, (struct sockaddr *)&address, sizeof(address));
     assert(ret != -1);
     ret = listen(sock, backlog);
     assert(ret != -1);
