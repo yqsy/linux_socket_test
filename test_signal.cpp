59c1d33a0e5bed08a99040b310db3ea511db2960
yqsy <yqsy021@126.com>
2017-11-11 23:48:38 +0800

    update

--- test_signal.cpp
+++ test_signal.cpp
@@ -1,10 +1,33 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
 #include <iostream>
 #include <chrono>
 #include <thread>
 
-int main( )
+void sig_hanadler(int singo)
 {
-    std::cout << "thread id: " << std::this_thread::get_id() << std::endl;
-    
+    if (singo == SIGINT)
+    {
+        std::cout << "recv SIGINT and thread id: " << std::this_thread::get_id() << std::endl;
+    }
+}
+
+int main()
+{
+    std::cout << "main thread id: " << std::this_thread::get_id() << std::endl;
+
+    if (signal(SIGINT, sig_hanadler) == SIG_ERR)
+    {
+        std::cout << "can't catch SIGINT\n";
+    }
+
+    while (1)
+    {
+        sleep(1);
+    }
+
+
     return 0;
 }
