887d10d5989059ac8d73bf11d8a3b838a8480de8
yqsy <yqsy021@126.com>
2017-11-26 20:42:31 +0800

    update

--- ttcp/ttcp_libev_test.cpp
+++ ttcp/ttcp_libev_test.cpp
@@ -1,7 +1,50 @@
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
 #include <ev.h>
 
+#include <memory>
+#include <vector>
+
+uint16_t PORT = 5002;
+
+int setnonblock(int fd) {
+  int flags = fcntl(fd, F_GETFL);
+  flags |= O_NONBLOCK;
+  return fcntl(fd, F_SETFL, flags);
+}
+
+int get_listen_fd(uint16_t port) {
+  int fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  assert(fd >= 0);
+
+  int rtn = setnonblock(fd);
+  assert(rtn != -1);
+
+  int yes = 1;
+  rtn = setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
+  assert(rtn == 0);
+
+  struct sockaddr_in addr;
+  bzero(&addr, sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(port);
+  addr.sin_addr.s_addr = INADDR_ANY;
+
+  rtn = bind(fd, (sockaddr *)(&addr), sizeof(addr));
+  assert(rtn == 0);
+
+  rtn = listen(fd, 5);
+  assert(rtn == 0);
+
+  return fd;
+}
 
 int main() {
-    
-    return 0;
+  int listenfd = get_listen_fd(PORT);
+
+  return 0;
 }
--- ttcp/ttcp_libev_test.h
+++ ttcp/ttcp_libev_test.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include <ev.h>
+
+#include <memory>
+#include <vector>
+
+struct EvClient {
+  ev_io io;
+  int fd;
+};
+
+typedef std::shared_ptr<EvClient> EvClientPtr;
