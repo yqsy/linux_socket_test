8ae296b608eab992428b5175831cf9cf2db856ce
yqsy <yqsy021@126.com>
2017-11-26 22:15:49 +0800

    失算,这是非阻塞IO啊

--- ttcp/ttcp_libev_test.cpp
+++ ttcp/ttcp_libev_test.cpp
@@ -12,9 +12,12 @@
 #include <memory>
 #include <vector>
 
+#include "common.h"
 #include "ttcp_libev_test.h"
 
 uint16_t PORT = 5002;
+
+const int MAX_RECEIVE_LENGTH = 1024 * 1024 * 10; // 10MB
 
 int setnonblock(int fd) {
   int flags = fcntl(fd, F_GETFL);
@@ -50,12 +53,56 @@ int get_listen_fd(uint16_t port) {
 
 static void client_cb(EV_P_ ev_io *w, int revents) {
   EvClient *ev_client = (EvClient *)w;
+
+  struct SessionMessage session_message = {0, 0};
+  int rb = read_n(ev_client->fd, &session_message, sizeof(session_message));
+  assert(rb = sizeof(session_message));
+
+  session_message.number = ntohl(session_message.number);
+  session_message.length = ntohl(session_message.length);
+  printf("receive number = %d\nreceived length = %d\n", session_message.number,
+         session_message.length);
+
+  assert(session_message.length <= MAX_RECEIVE_LENGTH);
+
+  const int total_len = int(sizeof(int32_t) + session_message.length);
+  PayloadMessage *payload = (PayloadMessage *)malloc(total_len);
+  assert(payload);
+
+  for (int i = 0; i < session_message.number; ++i) {
+    payload->length = 0;
+
+    rb = read_n(ev_client->fd, &payload->length, sizeof(payload->length));
+    assert(rb == sizeof(payload->length));
+
+    payload->length = ntohl(payload->length);
+    assert(payload->length == session_message.length);
+
+    rb = read_n(ev_client->fd, payload->data, payload->length);
+    assert(rb == payload->length);
+
+    int32_t ack = htonl(payload->length);
+
+    int wb = write_n(ev_client->fd, &ack, sizeof(ack));
+    assert(wb == sizeof(ack));
+  }
+
+  free(payload);
+  close(ev_client->fd);
+
+  // FIXME: O(n)
+  auto &ev_clients = ev_client->ev_server->ev_clients;
+  for (auto iter = ev_clients.begin(); iter != ev_clients.end(); ++iter) {
+    if (&**iter == ev_client) {
+      ev_clients.erase(iter);
+    }
+  }
 }
 
 static void server_cb(EV_P_ ev_io *w, int revents) {
   printf("server fd become readable\n");
 
-  EvClientPtr ev_client = EvClientPtr();
+  EvClientPtr ev_client = std::make_shared<EvClient>();
 
   EvServer *ev_server = (EvServer *)w;
   assert(ev_server->fd >= 0);
@@ -67,6 +114,7 @@ static void server_cb(EV_P_ ev_io *w, int revents) {
   assert(rtn != -1);
 
   ev_io_init(&ev_client->io, client_cb, ev_client->fd, EV_READ);
+  ev_io_start(EV_A_ & ev_client->io);
 }
 
 int main() {
