4705b6b0b5cc9f232812c065f6f1073a3d25f768
yqsy <yqsy021@126.com>
2017-12-19 18:02:32 +0800

    update

--- http/http_blocking.cpp
+++ http/http_blocking.cpp
@@ -12,20 +12,62 @@
 
 #include <iostream>
 #include <thread>
+#include <unordered_map>
 #include <vector>
 
 #include <muduo/base/Logging.h>
 #include <muduo/base/TimeZone.h>
 
+void raw_print(char c) {
+  const char *buf = NULL;
+  switch (c) {
+  case '\r':
+    buf = "\\r";
+    break;
+  case '\n':
+    buf = "\\n\n";
+    break;
+  }
+  if (buf) {
+    fwrite(buf, 1, strlen(buf), stdout);
+    return;
+  }
+
+  fwrite(&c, 1, sizeof(c), stdout);
+}
+
 void accept_request(int client_fd) {
   LOG_INFO << "new client_fd = " << client_fd;
 
-  char buf[1024];
-  int rn = read(client_fd, buf, sizeof(buf));
+  while (true) {
+    char c;
+    ssize_t nr = read(client_fd, &c, 1);
+    if (nr < 0) {
+      if (errno == EINTR) {
+        continue;
+      } else {
+        LOG_ERROR << "read" << strerror(errno);
+        break;
+      }
+    } else if (nr == 0) {
+      LOG_INFO << "eof client_fd = " << client_fd;
+      break;
+    } else {
+      raw_print(c);
+      // FIXME: move to buffer
+    }
+  }
 
-  //   int numchars = get_line(client_fd, buf, sizeof(buf));
-  //   LOG_INFO << numchars << " readed";
-  // close(client_fd);
+  if (::shutdown(client_fd, SHUT_WR) < 0) {
+    LOG_ERROR << "shutdown" << strerror(errno);
+  }
+
+  char buf[1024];
+  while (read(client_fd, buf, sizeof(buf)) > 0) {
+    // do nothing
+  }
+
+  LOG_INFO << "done client_fd = " << client_fd;
 }
 
 int get_listen_fd_or_die(uint16_t port) {
@@ -60,15 +102,20 @@ int get_listen_fd_or_die(uint16_t port) {
   return listenfd;
 }
 
-uint16_t PORT = 50000;
-
 int main(int argc, char *argv[]) {
   muduo::TimeZone beijing(8 * 3600, "CST");
   muduo::Logger::setTimeZone(beijing);
 
-  int listen_fd = get_listen_fd_or_die(PORT);
+  if (argc < 2) {
+    printf("Usage:\n%s port\n", argv[0]);
+    exit(1);
+  }
 
-  LOG_INFO << "listening on port = " << PORT;
+  uint16_t port = atoi(argv[1]);
+
+  int listen_fd = get_listen_fd_or_die(port);
+
+  LOG_INFO << "listening on port = " << port;
 
   while (1) {
     struct sockaddr_in peer_addr;
--- http/readme.md
+++ http/readme.md
@@ -1,6 +1,7 @@
 <!-- TOC -->
 
 - [1. 资料](#1-资料)
+- [2. 流程](#2-流程)
 
 <!-- /TOC -->
 
@@ -15,3 +16,17 @@
 * https://en.wikipedia.org/wiki/Berkeley_sockets (bsd sockets)
 * https://sourceforge.net/projects/tinyhttpd/ (tiny httpd)
 * http://man7.org/tlpi/code/online/dist/sockets/read_line.c.html (纯c的比较难写,readline)
+* https://www.jmarshall.com/easy/http/ (简单的http例子)
+
+<a id="markdown-2-流程" name="2-流程"></a>
+# 2. 流程
+
+1. Get your basic TCP sockets layer running (listen on port/ports, accept client connections and send/receive data).
+2. Implement a buffered reader so that you can read requests one line (delimited by CRLF) at a time.
+3. Read the very first line. Parse out the method, the request version and the path.
+4. Implement header parsing for the "Header: value" syntax. Don't forget unfolding folded headers.
+5. Check the request method, content type and content size to determine how/if the body will be read.
+6. Implement decoding of content based on content type.
+7. If you're going to support HTTP 1.1, implement things like "100 Continue", keep-alive, chunked transfer.
+8. Add robustness/security measures like detecting incomplete requests, limiting max number of clients etc.
+9. Shrink wrap your code and open-source it :)
