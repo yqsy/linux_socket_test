708bf968753d634a6b1742701116960a6b649645
yqsy <yqsy021@126.com>
2017-12-24 22:06:31 +0800

    update

--- procmon/tests/jinja_test.cpp
+++ procmon/tests/jinja_test.cpp
@@ -1,5 +1,10 @@
+#include <errno.h>
+
 #include <iostream>
 #include <string>
+
+#include <muduo/base/Logging.h>
+#include <muduo/base/TimeZone.h>
 
 #include <Jinja2CppLight/Jinja2CppLight.h>
 
@@ -8,17 +13,42 @@ using namespace Jinja2CppLight;
 
 int main(int argc, char *argv[])
 {
-  const std::string source = ("Hello {{name}}!"
-                              ""
-                              "{% if test %}"
-                              "    How are you?"
-                              "{% endif %}");
-  Template mytemplate(source);
-  mytemplate.setValue("name", "yqsy");
-  // mytemplate.setValue("test", 1);
+  muduo::TimeZone beijing(8 * 3600, "CST");
+  muduo::Logger::setTimeZone(beijing);
 
-  const string result = mytemplate.render();
-  std::cout << "[" << result << "]" << endl;
+  // const std::string source = ("Hello {{name}}!"
+  //                             ""
+  //                             "{% if test %}"
+  //                             "    How are you?"
+  //                             "{% endif %}");
+  // Template mytemplate(source);
+  // mytemplate.setValue("name", "yqsy");
+  // // mytemplate.setValue("test", 1);
+
+  // const string result = mytemplate.render();
+  // std::cout << "[" << result << "]" << endl;
+
+  FILE *fp = fopen("procmon/procmon.html", "rb");
+  if (fp)
+  {
+    const int kBufSize = 64 * 1024;
+    char buf[kBufSize];
+    size_t nread = fread(buf, 1, sizeof(buf), fp);
+
+    if (nread <= 0)
+    {
+      LOG_FATAL << "read <=0 " << strerror(errno);
+    }
+
+    Template mytemplate(buf);
+    mytemplate.setValue("procname", "proc1");
+    mytemplate.setValue("hostname", "host1");
+    std::cout << mytemplate.render();
+  }
+  else
+  {
+    LOG_FATAL << "can not open html " << strerror(errno);
+  }
 
   return 0;
 }
